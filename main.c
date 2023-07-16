eb4f5a7cda16f5b9000706b1a3b50900949d12b6
Denis <denis_kulikov_a_2003@mail.ru>
2023-07-17 02:11:52 +0700

    Program print "Hello world"

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "foo.h"
+
+int main ()
+{
+    f_print();
+
+    return 0;
+}
---
+++ print.c
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdio.h>
+
+void f_print ()
+{
+    printf("Hello world!\n");
+}    
